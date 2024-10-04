77cbfebc8ed81253a2c96c97d3f9848349974dd7
[example] <[bansalsneha991]>
2024-10-04 21:02:21 +0530

    new

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,11 @@ const config = {
             to: '/docs/lakshya',
             position: 'left'
           },
+          {
+            label: 'Sneha Bansal',
+            to: '/docs/sneha',
+            position: 'left'
+          },
           // {
           //   label: 'About',
           //   to: '/docs/about',
