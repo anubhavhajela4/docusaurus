497c49a08e49da95b41ba7417dbd283d5fd33392
iiviie <divyanshverma158@gmail.com>
2024-10-04 16:54:31 +0530

    update docusaurus.config.js, create divyansh.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,11 @@ const config = {
             to: '/docs/lakshya',
             position: 'left'
           },
+          {
+            label: 'Divyansh Verma',
+            to: '/docs/divyansh',
+            position: 'left'
+          },
           // {
           //   label: 'About',
           //   to: '/docs/about',
