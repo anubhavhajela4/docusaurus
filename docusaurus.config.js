68c6b05e7ea961c12c4da50e7fcd677bd21b99dc
Navya Gupta <navyagupta171@gmail.com>
2024-10-04 16:29:08 +0530

    my name is added to the navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,11 @@ const config = {
             to: '/docs/lakshya',
             position: 'left'
           },
+          {
+            label: 'Navya Gupta',
+            to: '/docs/navya',
+            position: 'left'
+          },
           // {
           //   label: 'About',
           //   to: '/docs/about',
