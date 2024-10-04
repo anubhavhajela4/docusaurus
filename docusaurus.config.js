0e79126c98f7ac71a5862804e7440f767b1a5265
Mrigank923 <mrigankraj23@gmail.com>
2024-10-05 01:01:52 +0530

    craeted a file mrigank and edit it to navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,11 @@ const config = {
             to: '/docs/lakshya',
             position: 'left'
           },
+          {
+            label: 'Mrigank',
+            to: '/docs/Mrigank',
+            position: 'left'
+          },
           // {
           //   label: 'About',
           //   to: '/docs/about',
