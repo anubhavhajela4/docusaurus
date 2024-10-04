e51b6564a755301aa5df539e63e9e3bddf66266a
srayansh <sr.gupta621@gmail.com>
2024-10-04 16:47:19 +0530

    Adding name in navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,6 +86,11 @@ const config = {
             to: '/docs/lakshya',
             position: 'left'
           },
+          {
+            label: 'Srayansh Gupta',
+            to: '/docs/srayansh',
+            position: 'left'
+          },
           // {
           //   label: 'About',
           //   to: '/docs/about',
